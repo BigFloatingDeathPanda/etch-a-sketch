7de6cad80fc9c1e712ced3ecd17c417c121239be
Nathan B <nabutler@gmail.com>
2020-05-19 19:17:26 -0400

    Big Bug Fixes

--- main2.js
+++ main2.js
@@ -30,63 +30,23 @@ function runResetFunction() {
     //const newHeight = document.getElementById("boxheight").value;
     createGrid(newWidth, newWidth);
     switchColor = document.querySelectorAll(".empty");
-    
     for (let i = 0; i < switchColor.length; i++) {
         switchColor[i].style.backgroundColor = `rgba(255, 255, 255, 1)`;
-    }
-
-    runSwitchColor(switchColor);    
+        let colorPicker = document.getElementsByName("colorType");  //Selects radio color, passes it to background color below
+        switchColor[i].addEventListener("mouseover", () => {
+            switchColor[i].classList.remove("empty");
+            switchColor[i].classList.add("mouseover");
+            switchColor[i].style.cssText = `background-color: ${colorDecisionFunction(colorPicker, i)}`;                
+        });
+    }   
 }
 
 function RANDOM(lower, upper) {
     return Math.floor(Math.random()*(upper - lower) + lower);//.toString(16).toUpperCase();
 };
 
-function runSwitchColor(switchColor) {
-    //let switchColor = document.querySelectorAll(".empty");
-    
-    let inputType = document.getElementById("mouse").checked;
-    if (inputType) {
-        inputType = "mouseover";
-    } else {
-        inputType = "click";
-    }    
-
-    let colorPicker = document.getElementsByName("colorType");  //Selects radio color, passes it to background color below
-
-    for (let i = 0; i<switchColor.length; i++) {
-        
-       //if (inputType == "mouseover") {
-       //     switchColor[i].removeEventListener("click", ());
-       // } else {
-       //     switchColor[i].removeEventListener("mouseover", ());
-       // };
-        
-       //All of these here comments were to try to get the 
-       //classChanger function below to work.  I still don't quite
-       // understand how to convert from arrow notation to a literal 
-       // function.  Maybe I'll come back to this project when I do.
-       // Note, above, with the "if" statement - I need to remove the event listener
-       // of whatever input type I'm not using.  removeEventListener only
-       // works with literal functions, not anonymous ones.
-       // Anyway.  classChanger() does not currently fucntion as it should.
-       // instead it changes all the classes to mouseover.
-        
-        //console.log(switchColor[i]);
-       // switchColor[i].addEventListener("mouseover", classChanger(i, switchColor));
-
-       
-        switchColor[i].addEventListener(inputType, () => {
-            switchColor[i].classList.remove("empty");
-            switchColor[i].classList.add("mouseover");
-            switchColor[i].style.cssText = `background-color: ${colorDecisionFunction(colorPicker, i)}`;
-        });
-    };
-};
-
-
 let hueAdd = 0;
-let lightRemove = 12;
+let lightRemove = 17;
 
 function colorDecisionFunction (colorPicker, i) {
     if (colorPicker[0].checked == true) { //Black
@@ -136,6 +96,10 @@ function colorDecisionFunction (colorPicker, i) {
             return `white`;
         }
 
+        if (currentShade == "black") {
+            return(`rgba(${lightRemove}, ${lightRemove}, ${lightRemove}, 1)`);
+        }
+
         console.log(`BORK ${currentShade}`);
                
         let currentShadeR = currentShade[0];
@@ -176,16 +140,6 @@ function colorDecisionFunction (colorPicker, i) {
         }
 
         return `rgba(${currentShadeR}, ${currentShadeG}, ${currentShadeB}, 1)`;
-
- 
- /* This color used to be "summer" but I didn't like it and couldn't distinguish it from "warm pallet"
-      let H;
-        do {
-            H = RANDOM(0, 360);
-            console.log(H);
-        } while (H > 40 && H < 295);
-        return `hsl(${H}, ${RANDOM(86, 101)}%, ${RANDOM(45, 60)}%)`;
- */   
     } else if (colorPicker[19].checked == true) { //Darken
 
         //Yeah this line right here, man.  What a mess.  Good luck following that one, future self!
@@ -202,7 +156,7 @@ function colorDecisionFunction (colorPicker, i) {
         if (currentShadeR == "") {
             currentShadeR = 255;
         }
-        currentShadeR = (currentShadeR - lightRemove);
+        currentShadeR = (Number(currentShadeR) - lightRemove);
         
         if (currentShadeR <= 0) {
             currentShadeR = 0;
@@ -213,7 +167,7 @@ function colorDecisionFunction (colorPicker, i) {
         if (currentShadeG == undefined) {
             currentShadeG = 255;
         } 
-        currentShadeG = (currentShadeG - lightRemove);
+        currentShadeG = (Number(currentShadeG) - lightRemove);
         
         if (currentShadeG <= 0) {
             currentShadeG = 0;
@@ -223,7 +177,7 @@ function colorDecisionFunction (colorPicker, i) {
         if (currentShadeB == undefined) {
             currentShadeB = 255;
         }
-        currentShadeB = (currentShadeB - lightRemove);
+        currentShadeB = (Number(currentShadeB) - lightRemove);
         
         if (currentShadeB <= 0) {
             currentShadeB = 0;
@@ -241,30 +195,5 @@ function classChanger(i, switchColor) {
     return switchColor;
 };
 
-
-createGrid(20, 20); 
 let switchColor = document.querySelectorAll(".empty");
-runSwitchColor(switchColor);
-
-const inputListener = document.getElementsByName("inputType");
-for (let i = 0; i<inputListener.length - 1; i++) {
-    inputListener[i].addEventListener("click", () => {
-        runSwitchColor(switchColor);
-    });
-};
-
-const colorListener = document.getElementsByName("colorType");
-for (let i = 0; i<colorListener.length - 1; i++) {
-    colorListener[i].addEventListener("click", () => {
-        runSwitchColor(switchColor);
-    });
-};
-
-
-
-//ToDo right here, figure out how to mouse over and change class of anything mousedover.  I think you need to select all and use a loop
-// but I don't quite remember why.  Check TOP DOM manipulation for ideas and details.  I pretty much know what I want, I just need
-//JS to understand what I wants
-
-     
-    
+runResetFunction(switchColor);
